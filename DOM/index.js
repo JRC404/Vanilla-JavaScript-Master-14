ca6b5f6955d84d3f7490b5e587c54fc089fa11d9
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-05 11:17:16 +0100

    DOM work updated

--- DOM/index.js
+++ DOM/index.js
@@ -17,10 +17,39 @@ const myFirstHeading = document.getElementById("heading");
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const paragraphs = document.getElementsByTagName("p"); // array
+const smallerHeading = document.getElementsByClassName("smallerHeading");
+// const evenSmallerHeading = document.getElementsByTagName()
+// const list = document.getElementsByTagName("li")
+const list = document.querySelector("li");
+// for (i = 0; i < list.length; i++) {
+//     list[i].style.color = "red";
+// }
 
-paragraphs[0].style.color = "red";
+list.style.color = "red";
+
+
+// paragraphs[0].style.color = "blue";
 // charmaine has said a loop... 
 // FOR arguments sake... if we were to use a loop...
+
+// loop through the elements... 
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
+
+paragraphs[0].style.color = "yellow";
+
+// change all the elements with the className smallerHeading... to any color you want - ARRAY... let's get all of the elements.
+
+for (let i = 0; i < smallerHeading.length; i++) {
+    smallerHeading[i].style.color = "pink";
+}
+
+document.getElementsByClassName('smallerHeading')[0].style.color='red'
+smallerHeading[1].style.color='green'
+
+
+
 
 
 myFirstHeading.addEventListener("click", () => {
@@ -30,7 +59,8 @@ myFirstHeading.addEventListener("click", () => {
 button.addEventListener("click", () => {
     // myFirstHeading.style.color = "yellow";
     myFirstHeading.style.color = input.value;
-    
+    // smallerHeading[0].style.color = input.value;
+    // console.log(input.value);
 })
 
 
